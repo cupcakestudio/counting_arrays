f7badfb5fc24793b136ce66327b13b7b9f3128db
Ting Hua Lin <coolting.lin3@gmail.com>
2023-02-07 19:38:05 +0100

    add program makes new line at every newly inserted item

--- script.js
+++ script.js
@@ -6,10 +6,18 @@ countArray();
 
 function countArray() {
   if (counter < 10) {
+    /*unshift takes the 1st element and shifts it, so the incoming element is the new first
+     element in the array, it adds elements to the beginning*/
     countingArr.unshift(`${counter}`);
-    // countingArr.shift(counter);
+    //put it in console
+    console.log(countingArr);
+    //increment by 1 (add a new item in array)
     counter++;
-    console.log(countingArr, counter);
+    //call countArray with delay
+    setTimeout(countArray, 100);
+    // countArray();
+  } else {
+    return;
   }
   // countArray();
   // setTimeout(countArray, 500);
