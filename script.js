feb515d8a628187d3f327f730c67c675954c497c
Ting Hua Lin <coolting.lin3@gmail.com>
2023-02-07 20:01:52 +0100

    makes sure that array.length stays at 9 items, by using pop() and unshift() items at the same time.

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let counter = 0;
 countArray();
 
 function countArray() {
-  if (counter < 10) {
+  if (countingArr.length < 9) {
     /*unshift takes the 1st element and shifts it, so the incoming element is the new first
      element in the array, it adds elements to the beginning*/
     countingArr.unshift(`${counter}`);
@@ -16,8 +16,20 @@ function countArray() {
     //call countArray with delay for 1 sec (1000 ms)
     setTimeout(countArray, 1000);
     // countArray();
-  } else {
-    return;
+  }
+  //whole else statement ensure that array only will have 9 items,
+  // since we both pop items out and unshifts new items into it at the same time
+  else {
+    //return/remove the last item from the array
+    countingArr.pop(`${counter}`);
+    //add next counted item into the beginning of the array
+    countingArr.unshift(`${counter}`);
+    //console log it
+    console.log(countingArr);
+    //increment by 1
+    counter++;
+    //call countArray with delay for 1 sec (1000 ms)
+    setTimeout(countArray, 1000);
   }
   // countArray();
   // setTimeout(countArray, 500);
